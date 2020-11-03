7c949f646c0d94d0f20664d2681b7965558a5063
Genius-Badger <32609230+Genius-Badger@users.noreply.github.com>
2020-11-03 02:47:26 -0600

    update gitignore

--- finalVer/sim/square_mpt_test_iputf_input/synopsys/vcsmx/vcsmx_setup.sh
+++
@@ -1,5 +0,0 @@
-
-
-vhdlan -xlrm "C:/Users/Shuai/Desktop/5_ver_1.0/rtl/FP/FP32_accum_sim/dspba_library_package.vhd"
-vhdlan -xlrm "C:/Users/Shuai/Desktop/5_ver_1.0/rtl/FP/FP32_accum_sim/dspba_library.vhd"        
-vhdlan -xlrm "C:/Users/Shuai/Desktop/5_ver_1.0/rtl/FP/FP32_accum_sim/FP32_accum.vhd"           
--- test/5_ver_1.0/sim/square_mpt_test_iputf_input/cadence/ncsim_setup.sh
+++
@@ -1,5 +0,0 @@
-
-
-ncvhdl -v93 "C:/Users/Shuai/Desktop/5_ver_1.0/rtl/FP/FP32_accum_sim/dspba_library_package.vhd"
-ncvhdl -v93 "C:/Users/Shuai/Desktop/5_ver_1.0/rtl/FP/FP32_accum_sim/dspba_library.vhd"        
-ncvhdl -v93 "C:/Users/Shuai/Desktop/5_ver_1.0/rtl/FP/FP32_accum_sim/FP32_accum.vhd"           
